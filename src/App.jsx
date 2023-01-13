2d924baabb847b4bc365f9a58f7b909181931cd5
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-13 16:14:41 +0100

    change routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { Homepage } from "./pages/Homepage/Homepage.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Category } from "./pages/Category/Category";
 import { ROUTES } from "./constants/index.js";
 import { FilmDetails } from "./components/FilmDetails/FilmDetails";
 import { Header } from "./components/Header/Header";
 import "./index.scss";
 
 function App() {
-  const { HOME_PAGE, CATEGORY_DETAILS, FILM_DETAILS } = ROUTES;
+  const { HOME_PAGE, FILM_DETAILS, FILMS } = ROUTES;
 
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path={HOME_PAGE} element={ <Homepage/>} />
+          <Route path={HOME_PAGE} element={ <Homepage />} />
+          <Route path={FILMS} element={ <Homepage />} />
           <Route path={FILM_DETAILS} element={<FilmDetails />} />
-          <Route path={CATEGORY_DETAILS} element={ <Category/>} />
         </Routes>
       </BrowserRouter>
     </div>
