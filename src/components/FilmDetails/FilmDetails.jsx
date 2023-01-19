ef2ef13cc4b3cf3762331b8375d7172220008b38
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 16:08:26 +0100

    create goBack function

--- src/components/FilmDetails/FilmDetails.jsx
+++ src/components/FilmDetails/FilmDetails.jsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch.js";
-
-import { BASE_URL, ROUTES } from "../../constants/index.js";
+import { BASE_URL_FILMS, ROUTES } from "../../constants/index.js";
 import { FilmDetailsChild } from "../FilmDetailsChild/FilmDetailsChild";
-import curvedSVG from "../../assets/svgs/curved-arrow-back-outline-icon.svg";
-
 import "./FilmDetails.scss";
 
 export const FilmDetails = () => {
-  const { id } = useParams();
-  const { isLoading, isError, fetchedData } = useFetch(`${BASE_URL}/${id}?populate=*`);
+  const { id, locale } = useParams();
+  const { isLoading, isError, fetchedData } = useFetch(`${BASE_URL_FILMS}/${id}?populate=*`);
 
-  if(isLoading) {
+  const navigate = useNavigate();
+  const goBack = () => navigate(`${ROUTES.FILMS}/${locale}`);
+
+  if (isLoading) {
     return <p>Loading...</p>
   }
 
-  if(isError) {
+  if (isError) {
     return console.error("error")
   }
 
   return (
     <>
-      <div className="return">
-        <Link to={`${ROUTES.FILMS}`}>
-          <img src={curvedSVG} alt="curved-arrow" />
-        </Link>
-      </div>
-      {!isLoading && fetchedData && <FilmDetailsChild fetchedData={fetchedData} />}
+      <button onClick={goBack} className="return">
+        <div className="return__arrow"/>
+      </button>
+      {!isLoading && fetchedData && <FilmDetailsChild fetchedData={fetchedData}/>}
     </>
   )
 }
