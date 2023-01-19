86a173324236df4974025087d20268e72f22b925
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 16:10:35 +0100

    add translations and navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./Header.scss";
+import { LanguageSwitcher } from "../LanguageSwitcher/LanguageSwitcher.jsx";
+import { FormattedMessage } from "react-intl";
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "../../constants/index.js";
+import { LanguageContext } from "../../context/LanguageProvider.jsx";
 
 export const Header = () => {
+  const { FILMS, REVIEWS, HOME_PAGE } = ROUTES;
+  const { locale } = useContext(LanguageContext);
+
   return (
     <header className="header">
-      <h3>Films collection</h3>
+      <h3>
+        <FormattedMessage
+          id={"header.title"}
+          defaultMessage={"Films collection"}
+        />
+      </h3>
+      <div className="navigation__menu">
+        <NavLink to={`${HOME_PAGE}${locale}`}>
+          <FormattedMessage
+            id={"header.homePage"}
+            defaultMessage={"HomePage"}
+          />
+        </NavLink>
+        <NavLink to={`${FILMS}/${locale}`}>
+          <FormattedMessage
+            id={"header.films"}
+            defaultMessage={"Films"}
+          />
+        </NavLink>
+        <NavLink to={`${REVIEWS}/${locale}`}>
+          <FormattedMessage
+            id={"header.reviews"}
+            defaultMessage={"Reviews"}
+          />
+        </NavLink>
+      </div>
+      <LanguageSwitcher/>
     </header>
   )
 }
