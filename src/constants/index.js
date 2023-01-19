fa2c08026d60eff9ec3a7ca799c93a761a93dd16
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 15:55:27 +0100

    add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,26 @@
-export const BASE_URL = "http://localhost:1337/api/films";
+export const BASE_URL_FILMS = "http://localhost:1337/api/films";
+export const BASE_URL_REVIEWS = "http://localhost:1337/api/reviews";
 
 export const ROUTES = {
   HOME_PAGE: "/",
   FILMS: "/films",
-  FILM_DETAILS: "/films/:id",
+  REVIEWS: "/reviews",
+  FILM_DETAILS: "/films/:locale/:id",
+  NOT_FOUND: "*",
 }
+
+export const languageOptions = [{
+  code: "en",
+  label: "English",
+  id: 1
+},
+{
+  code: "uk",
+  label: "Ukrainian",
+  id: 2
+},
+{
+  code: "pl",
+  label: "Polish",
+  id: 3
+}];
