0fb5a4b0e42cf24886809e7e20a92aa17c582f18
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 15:47:37 +0100

    add translates

---
+++ src/pages/FilmsPage/FilmsPage.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { Film } from "../../components/Film/Film.jsx";
+
+import "./Homepage.scss";
+import { BASE_URL_FILMS } from "../../constants/index.js";
+import { LanguageContext } from "../../context/LanguageProvider.jsx";
+
+export const Homepage = () => {
+  const { locale } = useContext(LanguageContext);
+  const { isLoading, isError, fetchedData } = useFetch(`${BASE_URL_FILMS}?locale=${locale}`);
+  const films = fetchedData?.data;
+
+  if(isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if(isError) {
+    return console.error("error")
+  }
+
+  return (
+    <div className="films">
+      {films?.map(({attributes, id}, index) => {
+        return <Film
+          key={id}
+          filmAttributes={attributes}
+          filmId={id}
+          filmIndex={index+1}
+        />
+      })}
+    </div>
+  )
+}
