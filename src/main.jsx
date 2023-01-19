93b804302867579b84f4bc8c161bf73fa3ec7d5b
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 15:58:15 +0100

    add wrappers: BtowserRouter and LanguageProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.scss";
+import { LanguageProvider } from "./context/LanguageProvider.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
