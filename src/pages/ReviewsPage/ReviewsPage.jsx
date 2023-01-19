179165167e442ce33f6ed957716809cfa87cf29e
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 15:54:50 +0100

    create scss file - ReviewsPage

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useFetch } from "../../hooks/useFetch.js";
+import { BASE_URL_REVIEWS } from "../../constants/index.js";
+import { useParams } from "react-router-dom";
 
-export const Reviews = () => {
-  return (
-    <div>Reviews</div>
-  )
+export const ReviewsPage = () => {
+  const { locale } = useParams();
+  const { fetchedData } = useFetch(`${BASE_URL_REVIEWS}?locale=${locale}`);
+  const reviews = fetchedData?.data;
+
+  return reviews?.map(({attributes, id}, index) => {
+    return (
+      <div key={id} className="review">
+        <span className="review__order">{index + 1}</span>
+        {attributes.review}
+      </div>
+    )
+  })
 }
