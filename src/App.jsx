9c5c9ae9ab64efc8d2a915d2833a8d9ce920a1e7
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 16:07:03 +0100

    change route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import { Homepage } from "./pages/Homepage/Homepage.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FilmsPage } from "./pages/FilmsPage/FilmsPage.jsx";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/index.js";
 import { FilmDetails } from "./components/FilmDetails/FilmDetails";
 import { Header } from "./components/Header/Header";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+import { ReviewsPage } from "./pages/ReviewsPage/ReviewsPage";
+import { Homepage } from "./pages/Homepage/Homepage";
 import "./index.scss";
 
 function App() {
-  const { HOME_PAGE, FILM_DETAILS, FILMS } = ROUTES;
+  const { FILMS, FILM_DETAILS, HOME_PAGE, NOT_FOUND, REVIEWS } = ROUTES;
 
   return (
     <div className="App">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path={HOME_PAGE} element={ <Homepage />} />
-          <Route path={FILMS} element={ <Homepage />} />
-          <Route path={FILM_DETAILS} element={<FilmDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path={NOT_FOUND} element={<NotFoundPage />}/>
+        <Route path={`${HOME_PAGE}/:locale`} element={<Homepage />}/>
+        <Route path={`${HOME_PAGE}/details`} element={<Navigate to={HOME_PAGE} replace />}/>
+        <Route path={`${FILMS}/:locale`} element={<FilmsPage />}/>
+        <Route path={FILM_DETAILS} element={<FilmDetails />}/>
+        <Route path={`${REVIEWS}/:locale`} element={<ReviewsPage />}/>
+      </Routes>
     </div>
   )
 }
