46ffa176f6148c12ac2091f47c296341d5a48d24
Alyona <alyona.ivanova.kh@gmail.com>
2023-01-19 16:06:27 +0100

    add check for error and loading

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import "./Review.css";
+import { useFetch } from "../../hooks/useFetch.js";
+import { BASE_URL_FILMS } from "../../constants/index.js";
 
 export const Review = ({ reviewData }) => {
   const { review, reviewer, publishedAt } = reviewData;
+  const { isLoading, isError } = useFetch(`${BASE_URL_FILMS}`);
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (isError) {
+    return console.error("error")
+  }
 
   return (
     <div className="review">
       <div className="review__reviewer">
         {reviewer}
       </div>
-      <div>{publishedAt.substring(0, 10)}</div>
+      <div>{publishedAt?.substring(0, 10)}</div>
       <div className="review__text">
         {review}
       </div>
